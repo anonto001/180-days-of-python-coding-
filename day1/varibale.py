24a4060cd95e6f570ce6d7ee9d8bfbf871ce2072
Anonto mia <128637095+anonto001@users.noreply.github.com>
2023-05-14 19:31:48 +0600

    variables

---
+++ day1/varibale.py
@@ -0,0 +1,12 @@
+# Assigning a value to a variable
+name = "Anonto "
+
+# Printing the variable
+print(name)  # Output: John
+
+# Modifying the variable
+name = "Ankon"
+
+# Printing the modified variable
+print(name)  # Output: Jane
+ 
