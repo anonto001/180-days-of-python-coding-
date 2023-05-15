e0b4b2365213f5b01e2ee4b4295cc560457158e7
Anonto mia <128637095+anonto001@users.noreply.github.com>
2023-05-16 02:27:46 +0600

    Add files via upload

---
+++ day2/main.py
@@ -0,0 +1,80 @@
+#1
+a = 5
+b = 3
+sum = a + b
+print(sum)
+#2
+a = 5
+b = 3
+mult = a*b
+print(mult)
+#3
+a = 5
+b = 3
+sub = a-b
+print(sub)
+#4
+a = 5
+b = 3
+div = a/b
+print(div)
+#5
+a = 5
+b = 3
+div_int = a // b
+print(div_int)
+#6
+a = 5
+b = 3
+mod = a % b
+print(mod)
+#7
+a = 5
+b = 3
+exp = a ** b
+print(exp)
+#8
+a = 5
+a += 1
+print(a)
+#9
+a = 5
+a -= 1
+print(a)
+#10
+a = -5
+abs_value = abs(a)
+print(abs_value)
+# another  basic arithmetic problem solved using Python
+
+num1 = 8
+num2 = 4
+
+# Addition
+sum = num1 + num2
+print("Sum:", sum)
+
+# Subtraction
+diff = num1 - num2
+print("Difference:", diff)
+
+# Multiplication
+product = num1 * num2
+print("Product:", product)
+
+# Division
+quotient = num1 / num2
+print("Quotient:", quotient)
+
+# Integer Division (Floor Division)
+quotient_int = num1 // num2
+print("Quotient (Integer Division):", quotient_int)
+
+# Modulo (Remainder)
+remainder = num1 % num2
+print("Remainder:", remainder)
+
+# Exponentiation
+exponent = num1 ** num2
+print("Exponentiation:", exponent)
+
