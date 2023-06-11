649c11dbc1e9546fd463042e1fb015b34bf4f983
Anonto mia <128637095+anonto001@users.noreply.github.com>
2023-06-11 13:41:53 +0600

    day3

---
+++ main.py
@@ -0,0 +1,17 @@
+#1 assign values to multiple variables in one line 
+a,b,c= "Orange","Banana","Cherry"
+print(a)
+print(b)
+print(c)
+
+#2 assign values to multiple variables in one line lets have aonether example i have 85,92 and 96 in physics ,chemistry and math so how i assign this value ?
+phy,chem,math="85","92","96"
+print(phy)
+print(chem)
+print(math)
+
+#lets have amother example you go for breakfast and the meal bil was 40 taka for juice ,30 for bread and 25 for egg-
+ju,br,eg="40","30","25"
+print(ju)
+print(br)
+print(eg)
